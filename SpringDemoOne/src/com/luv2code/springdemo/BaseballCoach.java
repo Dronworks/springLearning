cbe72450e770c65d272745cd29839d0df69fdda1
Andrey Dobrikov <andrejy601@gmail.com>
2021-12-29 13:31:20 +0200

    initial commit

---
+++ SpringDemoOne/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,7 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService fortuneSe
---
+++ SpringDemoOne/src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDayliPractice() {
+		return "Play with some chicks";
+	}
+
+}
