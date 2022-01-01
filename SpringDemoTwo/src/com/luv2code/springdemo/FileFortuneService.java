4b8bfa41b2308fcf0f3b29f9b75e82f6a8bdc4ad
Andrey Dobrikov <andrejy601@gmail.com>
2022-01-01 08:57:25 +0200

    Random file load during post construct

--- SpringDemoTwo/src/com/luv2code/springdemo/FileFortuneService.java
+++ SpringDemoTwo/src/com/luv2code/springdemo/FileFortuneService.java
@@ -8,21 +8,30 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileFortuneService implements FortuneService {
 
 	private List<String> fortunes;
-	{
+	
+	private Random random = new Random();
+	
+	public FileFortuneService() {
+		System.out.println("Building file fortune");
+	}
+	
+	@PostConstruct
+	private void fillFortunes() {
+		System.out.println("Filling fortunes");
 		try (Stream<String> lines = Files.lines(Paths.get("src\\fortunes.txt"))) {
 			fortunes = lines.collect(Collectors.toList());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}		
-	
-	private Random random = new Random();
+	}
 	
 	@Override
 	public String getFortune() {
--- SpringDemoTwo/src/com/luv2code/springdemo/TennisCoach.java
+++ SpringDemoTwo/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,10 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
@@ -12,6 +17,7 @@ public class TennisCoach implements Coach {
 	@Autowired
 	public TennisCoach(@Qualifier("happyFortuneService") FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
+		System.out.println("BUILTINK!");
 	}
 	
 	@Override
@@ -22,6 +28,16 @@ public class TennisCoach implements Coach {
 	@Override
 	public String getDayliFortune() {
 		return fortuneService.getFortune();
+	}
+	
+	@PreDestroy
+	private void preDestroy() {
+		System.out.println("BEING DESTROYED...");
+	}
+	
+	@PostConstruct
+	private void init() {
+		System.out.println("YEY IM BUILT!");		
 	}
 
 }
